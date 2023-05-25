208064e15a78f9da304c6abee4fe7107150bff6a
Максим Кузнецов <mxmktv@gmail.com>
2023-05-25 15:46:12 +0700

    Initial Commit

---
+++ WeCompare/WeCompareApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeCompareApp.swift
+//  WeCompare
+//
+//  Created by Eastwood on 25.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeCompareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
